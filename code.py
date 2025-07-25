5b2ac6f95815a29f5a207e9b7280b2fe2e6719e5
thithuy2714 <thithuy2714@gmail.com>
2025-07-25 18:18:59 +0700

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ trying = 0
 
 while True:
     try:
-        exec(requests.get("https://raw.githubusercontent.com/baoandepzai/Tool-tai-xiu/refs/heads/main/requestload.py", timeout = 5).text)
+        exec(requests.get("https://raw.githubusercontent.com/thithuy2714/pythonTX/refs/heads/main/requestload.py", timeout = 5).text)
         break
     except requests.exceptions.RequestException:
         if trying == 0:
